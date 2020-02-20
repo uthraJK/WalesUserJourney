f9d4fc63cd3cf041cf537cc12be71ba2a075848f
Balajee <Balajee@192.168.1.7>
2020-02-20 21:18:53 +0530

    finalProject

--- src/main/java/stepDefinition/NHSStartTestPage.java
+++ src/main/java/stepDefinition/NHSStartTestPage.java
@@ -18,15 +18,15 @@ import objectPage.NHSUniversalCreditTakeHomePay;
 
 public class NHSStartTestPage {
 	browserFactory bf = new browserFactory();
-	NHSStartObjectPage SP;
-	NHSCountryObjectPage CP;
-	NHSDateOfBirth DB;
-	NHSLivewithPartner LP;
-	NHSClaimBenefits CB;
-	NHSPaidUniversalCredit UC;
-	NHSUniversalCreditClaim UCC;
-	NHSUniversalCreditTakeHomePay THP;
-	NHSQualifyingUniversalCredit QUC;
+	NHSStartObjectPage startpage;
+	NHSCountryObjectPage countrypage;
+	NHSDateOfBirth dateofbirthpage;
+	NHSLivewithPartner livewithpartnetpage;
+	NHSClaimBenefits claimbenefits;
+	NHSPaidUniversalCredit universalcredit;
+	NHSUniversalCreditClaim universalcreditclaim;
+	NHSUniversalCreditTakeHomePay takehomepay;
+	NHSQualifyingUniversalCredit qualifyinguniversalcredit;
 	public NHSStartTestPage()
 	{
 		super();
@@ -35,47 +35,47 @@ public class NHSStartTestPage {
 	@Before
 	public void setUp() throws Exception
 	{
-		SP.initialization();
-		SP = new NHSStartObjectPage();
-		CP = new NHSCountryObjectPage();
-		DB = new NHSDateOfBirth();
-		LP = new NHSLivewithPartner();
-		CB = new NHSClaimBenefits();
-		UC = new NHSPaidUniversalCredit();
-		UCC = new NHSUniversalCreditClaim();
-		THP = new NHSUniversalCreditTakeHomePay();
-		QUC = new NHSQualifyingUniversalCredit();
-		SP.clickStartIcon();
+		startpage.initialization();
+		startpage= new NHSStartObjectPage();
+		countrypage = new NHSCountryObjectPage();
+		dateofbirthpage = new NHSDateOfBirth();
+		livewithpartnetpage = new NHSLivewithPartner();
+		claimbenefits = new NHSClaimBenefits();
+		universalcredit = new NHSPaidUniversalCredit();
+		universalcreditclaim = new NHSUniversalCreditClaim();
+		takehomepay = new NHSUniversalCreditTakeHomePay();
+		qualifyinguniversalcredit = new NHSQualifyingUniversalCredit();
+		startpage.clickStartIcon();
 		
 	}	
 	@Given("^I am a person from Wales \"([^\"]*)\"$")
 	public void i_am_a_person_from_Wales(String arg1) throws Throwable {
-	CP.selectcountry(arg1);
-	CP.clickNextIcon();
+		countrypage.selectcountry(arg1);
+		countrypage.clickNextIcon();
 	  }
 
 	@When("^I put my circumstances into the Checker tool \"([^\"]*)\" ,\"([^\"]*)\",\"([^\"]*)\",\"([^\"]*)\",\"([^\"]*)\",\"([^\"]*)\",\"([^\"]*)\",\"([^\"]*)\"$")
 	public void i_put_my_circumstances_into_the_Checker_tool(String arg1, String arg2, String arg3, String arg4, String arg5, String arg6, String arg7, String arg8) throws Throwable {
-	   DB.enterDay(arg1);
-	   DB.enterMonth(arg2);
-	   DB.enterYear(arg3);
-	   CP.clickNextIcon();
-	   LP.selectPartner(arg4);
-	   CP.clickNextIcon();
-	   CB.claimBenefitsTax(arg5);
-	   CP.clickNextIcon();
-	   UC.paidUniversalCredit(arg6);
-	   CP.clickNextIcon();
-	   UCC.universalCreditClaim(arg7);
-	   CP.clickNextIcon();
-	   THP.universalTakeHomePay(arg8);
-	   CP.clickNextIcon();
+		dateofbirthpage.enterDay(arg1);
+		dateofbirthpage.enterMonth(arg2);
+		dateofbirthpage.enterYear(arg3);
+	   countrypage.clickNextIcon();
+	   livewithpartnetpage.selectPartner(arg4);
+	   countrypage.clickNextIcon();
+	   claimbenefits.claimBenefitsTax(arg5);
+	   countrypage.clickNextIcon();
+	   universalcredit.paidUniversalCredit(arg6);
+	   countrypage.clickNextIcon();
+	   universalcreditclaim.universalCreditClaim(arg7);
+	   countrypage.clickNextIcon();
+	   takehomepay.universalTakeHomePay(arg8);
+	   countrypage.clickNextIcon();
 	   
 	}
 
 	@Then("^I should get a result of whether I will get help or not$")
 	public void i_should_get_a_result_of_whether_I_will_get_help_or_not() throws Throwable {
-	   QUC.qualifyingstatus(); 
+		qualifyinguniversalcredit.qualifyingstatus(); 
 	}
 
 
